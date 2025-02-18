e90f5490e25e5360ec61a45177ff324da555c551
aturtle4 <armaansingh0404@gmail.com>
2025-02-19 00:50:32 +0530

    feat: added components for home login and signup with valid routing for them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>LeapCode</h1>
+      <Routes>
+        <Route path = '/' element = {<Navigate to='/login' />} />
+        <Route path = '/login' element = {<Login />} />
+        <Route path = '/signUp' element = {<SignUp />} />
+        <Route path = '/home' element = {<Home />} />
+      </Routes>
     </div>
   );
 }
